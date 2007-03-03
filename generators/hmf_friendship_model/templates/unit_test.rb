caa3f68afa4e9f8838c8cdbe44a4bb01b526b22b
Steve Ehrenberg <tehdnite@gmail.com>
2007-03-03 19:47:16 +0000

    small typo in the fixture template

--- generators/hmf_friendship_model/templates/unit_test.rb
+++ generators/hmf_friendship_model/templates/unit_test.rb
@@ -3,6 +3,9 @@ require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_hel
 class <%= class_name %>Test < Test::Unit::TestCase
   fixtures :<%= table_name %>
 
+  # I can't think of a whole lot to test here. Most of the tests 
+  # will be in the user unit test. If you think of anything I should
+  # put here, feel free to let me know.
   def test_truth
     assert true
   end
